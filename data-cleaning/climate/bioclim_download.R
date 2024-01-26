d780a4dcb1fc64335ca503da5e8d6da59d0669a2
shmuir <shmuir1@gmail.com>
2024-01-26 04:39:03 +0000

    bioclim data download

---
+++ data-cleaning/climate/bioclim_download.R
@@ -0,0 +1,49 @@
+
+## DO NOT NEED TO RUN THIS SCRIPT AGAIN- BIOCLIM HAS BEEN DOWNLOADED AS bioclim.tif
+## use plotting code and bioclim names as references 
+
+library(raster)
+#devtools::install_github("kapitzas/WorldClimTiles")
+library(WorldClimTiles)
+
+#Download tiles
+tilenames <- c("11","12","21","22")
+tiles <- tile_get(tilenames, "bio")
+tiles
+#Merge the downloaded tiles layer by layer. The function produces a raster stack that contains the merged layers with the correct variable names.
+merged <- tile_merge(tiles)
+
+plot(merged[[5]]) #Plotting bioclim 1 - much better!
+
+#Let's change the names to have a better defined raster
+bioclim_names <-c("Annual_Mean_Temp",
+                  "Mean_Diurnal_Range",
+                  "Isothermality",
+                  "Temp_Seasonality",
+                  "Max_Temp_Warmest Month",
+                  "Min_Temp_Coldest_Month",
+                  "Temp_Annual_Range",
+                  "Mean_Temp_Wettest_Quarter",
+                  "Mean_Temp_Driest_Quarter",
+                  "Mean_Temp_Warmest_Quarter",
+                  "Mean_Temp_Coldest_Quarter",
+                  "Annual_Precip",
+                  "Precip_Wettest_Month",
+                  "Precip_Driest_Month",
+                  "Precip_Seasonality",
+                  "Precip_Wettest_Quarter",
+                  "Precip_Driest_Quarter",
+                  "Precip_Warmest_Quarter",
+                  "Precip_Coldest_Quarter"
+)
+
+names(merged) <- bioclim_names
+
+#We can look in more detail at the rasters within the stack 
+plot(merged[[1]], main = names(merged[[1]]))
+
+#Lets save your environmental data as a GeoTIFF file
+outfile <- writeRaster(merged, filename='bioclim.tif', format="GTiff", overwrite=TRUE,options=c("INTERLEAVE=BAND","COMPRESS=LZW"))
+
+#if you want to read back your environmental RasterBrick data
+envdata <- brick("bioclim.tif")
