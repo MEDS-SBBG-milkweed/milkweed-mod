8b1021764d1dfdb289d3454e46e2280e8a87b8e2
shmuir <shmuir1@gmail.com>
2024-02-15 04:39:04 +0000

    milkweed map

---
+++ data-cleaning/milkweed-polygon/survey_points_map.R
@@ -0,0 +1,55 @@
+# load libraries ----
+library(tidyverse)
+library(janitor)
+library(sf)
+library(ggspatial)
+library(here)
+library(leaflet)
+library(basemaps)
+
+# read in data ----
+
+milkweed_data_raw <- st_read(here("~/../../capstone/milkweedmod/data/milkweed_polygon_data/"))
+
+# California National Forest boundaries
+boundary <- st_read(here("~/../../capstone/milkweedmod/data/lpnf_boundary_data/S_USA_AdministrativeForest.gdb/"))
+trails <- st_read(here("~/../../capstone/milkweedmod/data/2023_Regional_Trails_and_Roads_lines/2023_Regional_Trails_and_Roads_lines.shp"))
+
+# filter boundary
+lpnf_boundary <- boundary %>% 
+  filter(FORESTNAME %in% c("Los Padres National Forest")) 
+
+# filter data for mapping
+milkweed_map <- milkweed_data_raw |> 
+  janitor::clean_names() |> 
+  st_transform(crs(envs_Ac)) %>%
+  dplyr::select(milkweed_p, milkweed_sp) %>%
+  st_centroid() %>%
+  st_transform(crs(lpnf_boundary))
+
+milkweed_yes <- milkweed_map %>%
+  filter(milkweed_p == "yes")
+
+
+# map data
+colors <- c("#AC171E", "#EBAB9A", "#EFC103", "#5B6530", "#9483B1")
+pal <- colorFactor(colors, domain = milkweed_yes$milkweed_sp, reverse = TRUE)
+
+
+leaflet() %>% addProviderTiles(providers$Esri.WorldTopoMap) %>%
+  addCircleMarkers(data = milkweed_yes,
+                   radius = 2, 
+                   color = ~pal(milkweed_yes$milkweed_sp), 
+                   fill = TRUE, 
+                   fillColor = ~pal(milkweed_yes$milkweed_sp),
+                   opacity = 0.8) %>%
+  addPolygons(data = lpnf_boundary,
+              fill = FALSE,
+              color = "black",
+              weight = 2) %>%
+  leaflet::addLegend("topright", pal = pal, values = milkweed_yes$milkweed_sp,
+                     title = "Milkweed Species",
+                     opacity = 0.8) %>%
+  addScaleBar()
+
+
