63becf5e0c57169ea206ce56931ec0e2f9a09f9c
shmuir <shmuir1@gmail.com>
2024-03-22 19:25:49 +0000

    data cleaning set up

---
+++ data-cleaning/milkweed-polygon/points_clean_export.R
@@ -0,0 +1,119 @@
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                                                            --
+##------------------- MILKWEED POINTS CLEANING AND EXPORT-----------------------
+##                                                                            --
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Load packages
+library(tidyverse)
+library(sf)
+
+# Read in poygon data
+milkweed_data_raw <- st_read(here::here("~/../../capstone/milkweedmod/data/milkweed_polygon_data/"))
+
+# Filter and project
+milkweed_presence <- milkweed_data_raw |> 
+  janitor::clean_names() |> 
+  filter(milkweed_p != "no")  %>%
+  st_transform("EPSG:4326") 
+
+##~~~~~~~~~~~~~~~~~~~~~
+##  ~ Californica  ----
+##~~~~~~~~~~~~~~~~~~~~~
+
+# remove "No" observations
+californica <- milkweed_presence %>%
+  filter(milkweed_sp == "Asclepias californica") %>%
+  dplyr::select(milkweed_sp)
+
+# make points
+multi_p_cali <- st_cast(californica, "MULTIPOINT")
+cali_points <- multi_p_cali  %>% st_cast("POINT")
+
+cali_points <- cali_points %>%
+  st_coordinates() %>%
+  data.frame() %>%
+  mutate(scientific_name = "Asclepias californica") %>%
+  mutate(occID = row_number()) %>%
+  rename(longitude = X,
+         latitude = Y)
+
+
+##~~~~~~~~~~~~~~~~~~~~~
+##  ~ Vestita  ----
+##~~~~~~~~~~~~~~~~~~~~~
+
+# remove "No" observations
+vestita <- milkweed_presence %>%
+  filter(milkweed_sp == "Asclepias vestita") %>%
+  dplyr::select(milkweed_sp)
+
+# make points
+multi_p_vestita <- st_cast(vestita, "MULTIPOINT")
+vestita_points <- multi_p_vestita  %>% st_cast("POINT")
+
+vestita_points <- vestita_points %>%
+  st_coordinates() %>%
+  data.frame() %>%
+  mutate(scientific_name = "Asclepias vestita") %>%
+  mutate(occID = row_number()) %>%
+  rename(longitude = X,
+         latitude = Y)
+
+##~~~~~~~~~~~~~~~~~~~~~
+##  ~ Erosa  ----
+##~~~~~~~~~~~~~~~~~~~~~
+
+# remove "No" observations
+erosa <- milkweed_presence %>%
+  filter(milkweed_sp == "Asclepias erosa") %>%
+  dplyr::select(milkweed_sp)
+
+# make points
+multi_p_erosa <- st_cast(erosa, "MULTIPOINT")
+erosa_points <- multi_p_erosa  %>% st_cast("POINT")
+
+erosa_points <- erosa_points %>%
+  st_coordinates() %>%
+  data.frame() %>%
+  mutate(scientific_name = "Asclepias erosa") %>%
+  mutate(occID = row_number()) %>%
+  rename(longitude = X,
+         latitude = Y)
+
+##~~~~~~~~~~~~~~~~~~~~~
+##  ~ eriocarpa  ----
+##~~~~~~~~~~~~~~~~~~~~~
+
+# remove "No" observations
+eriocarpa <- milkweed_presence %>%
+  filter(milkweed_sp == "Asclepias eriocarpa") %>%
+  dplyr::select(milkweed_sp)
+
+# make points
+multi_p_eriocarpa <- st_cast(eriocarpa, "MULTIPOINT")
+eriocarpa_points <- multi_p_eriocarpa  %>% st_cast("POINT")
+
+eriocarpa_points <- eriocarpa_points %>%
+  st_coordinates() %>%
+  data.frame() %>%
+  mutate(scientific_name = "Asclepias eriocarpa") %>%
+  mutate(occID = row_number()) %>%
+  rename(longitude = X,
+         latitude = Y)
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                              Export to Taylor                            ----
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# write_csv(cali_points, "~/../../capstone/milkweedmod/data/clean_data/milkweed_points/californica_points.csv")
+# write_csv(erosa_points, "~/../../capstone/milkweedmod/data/clean_data/milkweed_points/erosa_points.csv")
+# write_csv(eriocarpa_points, "~/../../capstone/milkweedmod/data/clean_data/milkweed_points/eriocarpa_points.csv")
+# write_csv(vestita_points, "~/../../capstone/milkweedmod/data/clean_data/milkweed_points/vestita_points.csv")
+
+
+
+
+
+
--- scripts/setup.R
+++ scripts/setup.R
@@ -1,9 +1,55 @@
-# packages needed to read in data
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                                                            --
+##----------------------------- MILKWEEDMOD SETUP-------------------------------
+##                                                                            --
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
+##~~~~~~~~~~~~~~~~~~
+##  ~ Packages  ----
+##~~~~~~~~~~~~~~~~~~
 library(tidyverse)
 library(raster)
 library(terra)
 library(sf)
 
 
-# read in all cleaned data here
+##~~~~~~~~~~~~~~~~~~~~~~
+##  ~ Cleaned Data  ----
+##~~~~~~~~~~~~~~~~~~~~~~
+
+#.............................Milkweed Points....................
+
+californica_points <- read_csv("~/../../capstone/milkweedmod/data/clean_data/milkweed_points/californica_points.csv")
+erosa_points <- read_csv("~/../../capstone/milkweedmod/data/clean_data/milkweed_points/erosa_points.csv")
+eriocarpa_points <- read_csv("~/../../capstone/milkweedmod/data/clean_data/milkweed_points/eriocarpa_points.csv")
+vestita_points <- read_csv("~/../../capstone/milkweedmod/data/clean_data/milkweed_points/vestita_points.csv")
+
+#.............................Maxent.............................
+
+# Stacked rasters
+envs_Ac <- brick(here::here(("~/../../capstone/milkweedmod/data/clean_data/env_rasters/env_stack.tif")))
+envs_north <- brick(here::here(("~/../../capstone/milkweedmod/data/clean_data/env_rasters/env_stack_north.tif")))
+
+# Individual Layers
+bioclim <- brick(here::here("~/../../capstone/milkweedmod/data/bioclim/wallace_bioclim.tif"))
+canopy <- raster(here::here("~/../../capstone/milkweedmod/data/canopy_cover/canopy_cover_cleaned.tif"))
+slope <- raster(here::here("~/../../capstone/milkweedmod/data/dem/lpnf_slope.tif"))
+aspect <- raster(here::here("~/../../capstone/milkweedmod/data/dem/lpnf_aspect.tif"))
+
+#.............................LPNF Boundary.............................
+
+lpnf_boundary <- st_read(here("~/../../capstone/milkweedmod/data/lpnf_boundary_data/lpnf_boundary/lpnf_boundary.shp"))
+lpnf_north <- st_read(here("~/../../capstone/milkweedmod/data/lpnf_boundary_data/lpnf_boundary_north/lpnf_boundary_north.shp")) 
+# %>%
+#   st_buffer(dist = 1000) # add buffer if necessary
+lpnf_south <- st_read(here("~/../../capstone/milkweedmod/data/lpnf_boundary_data/lpnf_boundary_south/lpnf_boundary_south.shp"))
+# %>%
+#   st_buffer(dist = 1000) # add buffer if necessary
+
+
+#.............................Land Ownership.............................
+
+
+#.............................Trails and Roads.............................
+
