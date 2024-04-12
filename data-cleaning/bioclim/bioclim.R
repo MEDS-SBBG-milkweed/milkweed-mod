9c28e0466eddbb0ee2d21a1034b73c3735aed126
shmuir <shmuir1@gmail.com>
2024-04-12 21:10:11 +0000

    renaming folders; edit file structures

---
+++ data-cleaning/bioclim/bioclim.R
@@ -0,0 +1,19 @@
+library(wallace)
+
+# Download environmental data
+envs_Ac_E <- envs_worldclim(
+  bcRes = 0.5, # resolution 30arc seconds
+  bcSel = c('bio01', 'bio02', 'bio03', 'bio04', 'bio05', 'bio06', 'bio07', 'bio08', 'bio09', 'bio10', 'bio11', 'bio12', 'bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19'), # select all bioclim vars
+   mapCntr = c(-119.658, 34.909), # Mandatory for 30 arcsec resolution - might need to adjust
+  doBrick = FALSE)
+
+envs_Ac_W <- envs_worldclim(
+  bcRes = 0.5, # resolution 30arc seconds
+  bcSel = c('bio01', 'bio02', 'bio03', 'bio04', 'bio05', 'bio06', 'bio07', 'bio08', 'bio09', 'bio10', 'bio11', 'bio12', 'bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19'), # select all bioclim vars
+   mapCntr = c(-120.478, 38.271), # Mandatory for 30 arcsec resolution - might need to adjust
+  doBrick = FALSE)
+
+#Mosaic the two tiles together to get whole study region
+envs_Ac <- terra::mosaic(envs_Ac_W, envs_Ac_E, fun = "mean")
+
+#writeRaster(envs_Ac, here::here("~/../../capstone/milkweedmod/clean_data/bioclim/wallace_bioclim.tif"))
