8301319ca361056108950c35b186b8187c83adb5
shmuir <shmuir1@gmail.com>
2024-04-17 16:34:42 +0000

    modeling with erosa -- looking bad

--- scripts/setup.R
+++ scripts/setup.R
@@ -45,5 +45,7 @@ lpnf_boundary <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boun
 lpnf_boundary_buffered <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_buffered/lpnf_boundary_buffered.shp"))
 lpnf_north <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_north/lpnf_boundary_north.shp")) 
 lpnf_north_buffered <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_north_buffered/lpnf_boundary_north_buffered.shp"))
-lpnf_south <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_south/lpnf_boundary_south.shp"))
+lpnf_south <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_south/lpnf_boundary_south.shp")) 
+# %>%
+#   st_buffer(dist = 500)
 lpnf_south_buffered <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_south_buffered/lpnf_boundary_south_buffered.shp"))
