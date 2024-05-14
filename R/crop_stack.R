d82290d4304822c8c5010bac68cc52ffd49fcb8e
amandaherbst <amandaeherbst@gmail.com>
2024-05-14 17:35:10 +0000

    deleting unecessary and unused files and lines

--- R/crop_stack.R
+++ R/crop_stack.R
@@ -10,12 +10,6 @@ canopy <- raster(here::here("~/../../capstone/milkweedmod/clean_data/canopy_cove
 northness <- raster(here::here("~/../../capstone/milkweedmod/clean_data/dem/northness.tif"))
 eastness <- raster(here::here("~/../../capstone/milkweedmod/clean_data/dem/eastness.tif"))
 
-# California National Forest boundaries
-lpnf_boundary <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary/lpnf_boundary.shp"))
-lpnf_north_buffer <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_north/lpnf_boundary_north.shp")) %>%
-  st_buffer(dist = 1000)
-lpnf_south_buffer <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_south/lpnf_boundary_south.shp")) %>%
-  st_buffer(dist = 1000)
 
 # Resample
 bioclim_resample <- resample(bioclim, canopy)
