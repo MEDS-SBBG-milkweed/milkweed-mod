0f526ad6ff1845e152303a6a0bdd9399288d410b
shmuir <shmuir1@gmail.com>
2024-06-04 16:48:21 +0000

    updating read in file paths

--- R/setup.R
+++ R/setup.R
@@ -4,6 +4,8 @@
 ##                                                                            --
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+# Define base directory file path
+base_dir <- here::here("~/Library/CloudStorage/Box-Box/MEDS-SBBG-milkweed")
 
 ##~~~~~~~~~~~~~~~~~~
 ##  ~ Packages  ----
@@ -12,6 +14,7 @@ library(tidyverse)
 library(raster)
 library(terra)
 library(sf)
+library(here)
 
 
 ##~~~~~~~~~~~~~~~~~~~~~~
@@ -20,27 +23,21 @@ library(sf)
 
 #.............................Milkweed Points....................
 
-californica_points <- read_csv("~/../../capstone/milkweedmod/clean_data/milkweed_data/sdm_milkweed_points/californica_points.csv")
-erosa_points <- read_csv("~/../../capstone/milkweedmod/clean_data/milkweed_data/sdm_milkweed_points/erosa_points.csv")
-eriocarpa_points <- read_csv("~/../../capstone/milkweedmod/clean_data/milkweed_data/sdm_milkweed_points/eriocarpa_points.csv")
-vestita_points <- read_csv("~/../../capstone/milkweedmod/clean_data/milkweed_data/sdm_milkweed_points/vestita_points.csv")
+californica_points <- read_csv(here(base_dir, "clean_data", "milkweed_data", "sdm_milkweed_points", "californica_points.csv"))
+erosa_points <- read_csv(here(base_dir, "clean_data", "milkweed_data", "sdm_milkweed_points", "erosa_points.csv"))
+eriocarpa_points <- read_csv(here(base_dir, "clean_data", "milkweed_data", "sdm_milkweed_points", "eriocarpa_points.csv"))
+vestita_points <- read_csv(here(base_dir, "clean_data", "milkweed_data", "sdm_milkweed_points", "vestita_points.csv"))
 
 #.............................Maxent.............................
 
 # Stacked rasters
-envs_Ac <- brick(here::here(("~/../../capstone/milkweedmod/clean_data/sdm_env_stack/env_stack.tif")))
-
-# Individual Layers
-# bioclim <- brick(here::here("~/../../capstone/milkweedmod/clean_data/bioclim/wallace_bioclim.tif"))
-# canopy <- raster(here::here("~/../../capstone/milkweedmod/clean_data/canopy_cover/canopy_cover_cleaned.tif"))
-# slope <- raster(here::here("~/../../capstone/milkweedmod/clean_data/dem/lpnf_slope.tif"))
-# aspect <- raster(here::here("~/../../capstone/milkweedmod/clean_data/dem/lpnf_aspect.tif"))
+envs_Ac <- brick(here(base_dir, "clean_data", "sdm_env_stack", "env_stack.tif"))
 
 #.............................LPNF Boundary.............................
 
-lpnf_boundary <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary/lpnf_boundary.shp"))
-lpnf_boundary_buffered <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_buffered/lpnf_boundary_buffered.shp"))
-lpnf_north <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_north/lpnf_boundary_north.shp")) 
-lpnf_north_buffered <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_north_buffered/lpnf_boundary_north_buffered.shp"))
-lpnf_south <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_south/lpnf_boundary_south.shp")) 
-lpnf_south_buffered <- st_read(here("~/../../capstone/milkweedmod/clean_data/lpnf_boundary/lpnf_boundary_south_buffered/lpnf_boundary_south_buffered.shp")) 
+lpnf_boundary <- st_read(here(base_dir, "clean_data", "lpnf_boundary", "lpnf_boundary", "lpnf_boundary.shp"))
+lpnf_boundary_buffered <- st_read(here(base_dir, "clean_data", "lpnf_boundary", "lpnf_boundary_buffered", "lpnf_boundary_buffered.shp"))
+lpnf_north <- st_read(here(base_dir, "clean_data", "lpnf_boundary", "lpnf_boundary_north", "lpnf_boundary_north.shp")) 
+lpnf_north_buffered <- st_read(here(base_dir, "clean_data", "lpnf_boundary", "lpnf_boundary_north_buffered", "lpnf_boundary_north_buffered.shp"))
+lpnf_south <- st_read(here(base_dir, "clean_data", "lpnf_boundary", "lpnf_boundary_south", "lpnf_boundary_south.shp")) 
+lpnf_south_buffered <- st_read(here(base_dir, "clean_data", "lpnf_boundary", "lpnf_boundary_south_buffered", "lpnf_boundary_south_buffered.shp")) 
